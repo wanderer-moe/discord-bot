b60391a57f18530ea729e294544bfab731ddceb6
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-07-04 19:13:38 +0100

    create working `/recent` command

--- src/commands/recent.rs
+++ src/commands/recent.rs
@@ -2,6 +2,7 @@ use crate::command::{Command, CommandInput};
 use crate::error::InteractionError;
 use crate::interaction::{ApplicationCommandOption, InteractionApplicationCommandCallbackData};
 use async_trait::async_trait;
+use reqwest::Client;
 
 pub(crate) struct Recent {}
 
@@ -11,8 +12,34 @@ impl Command for Recent {
         &self,
         _input: &CommandInput,
     ) -> Result<InteractionApplicationCommandCallbackData, InteractionError> {
+        let client = Client::new();
+        let response = client
+            .get("https://v2-api-testing.wanderer.moe/recent")
+            .send()
+            .await;
+        let content = match response {
+            Ok(response) => {
+                let json = response.json::<serde_json::Value>().await.unwrap();
+                let mut content = String::new();
+                if let Some(assets) = json["results"].as_array() {
+                    let mut i = 1;
+                    for asset in assets.iter().take(10) {
+                        content.push_str(&format!(
+                            "{}. {}\n",
+                            i,
+                            asset["name"].as_str().unwrap_or("Unknown")
+                        ));
+                        i += 1;
+                    }
+                } else {
+                    content = "Error: No results".to_string();
+                }
+                content
+            }
+            Err(e) => format!("Error fetching recent assets: {}", e),
+        };
         Ok(InteractionApplicationCommandCallbackData {
-            content: Some("Test Response".to_string()),
+            content: Some(content),
             choices: None,
             embeds: None,
         })
@@ -23,7 +50,7 @@ impl Command for Recent {
     }
 
     fn description(&self) -> String {
-        "Get top 30 recently uploaded assets".into()
+        "Get top 10 recently uploaded assets".into()
     }
 
     fn options(&self) -> Option<Vec<ApplicationCommandOption>> {
